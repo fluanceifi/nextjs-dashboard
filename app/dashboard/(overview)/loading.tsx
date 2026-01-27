7a6864a73775984a5bead07d3390302eac02a24e
FluanceIFI <gksrnr66@gmail.com>
2026-01-27 23:36:11 +0900

    .
    final

--- app/dashboard/(overview)/loading.tsx
+++ app/dashboard/(overview)/loading.tsx
@@ -1,5 +1,4 @@
-import DashboardSkeleton from '@/app/ui/skeletons';
-
+import DashboardSkeleton from '@/nextjs-dashboard/app/ui/skeletons';
 export default function Loading() {
     return <DashboardSkeleton />;
 }
